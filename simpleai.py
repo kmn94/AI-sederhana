c863e1aa988c22404614aba8aae3510682c12bbe
kmn94 <91653045+kmn94@users.noreply.github.com>
2021-11-18 06:56:06 +0700

    Add files via upload

---
+++ simpleai.py
@@ -0,0 +1,24 @@
+import time
+
+print("AI:siapa namamu?")
+nama = input("kamu: ")
+print(" ")
+
+print("AI:dimana negaramu?")
+negara = input("kamu: ")
+print(" ")
+
+print("AI:apa hobimu?")
+hobi = input("kamu: ")
+print(" ")
+
+print(".")
+time.sleep(2)
+print("..")
+time.sleep(3)
+print("...")
+time.sleep(3)
+
+print("AI:namamu adalah",nama)
+print("AI:kamu dari negara",negara)
+print("AI:hobi mu adalah",hobi)
